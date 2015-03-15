a716da784b11388227ca35aa6ed2bc9487b76c62
TomKfr <tkieffer67@gmail.com>
2015-03-15 20:50:28 +0100

    l

--- plugins/vocal_infos/testfb.php
+++ plugins/vocal_infos/testfb.php
@@ -6,13 +6,13 @@ require __DIR__ . '/Facebook/autoload.php';
  require_once( 'Facebook/FacebookSession.php' );
  require_once( 'Facebook/Entities/AccessToken.php' );
 // require_once( 'Facebook/FacebookRedirectLoginHelper.php' );
-// require_once( 'Facebook/FacebookRequest.php' );
+ require_once( 'Facebook/FacebookRequest.php' );
 // require_once( 'Facebook/FacebookResponse.php' );
 // require_once( 'Facebook/FacebookSDKException.php' );
-// require_once( 'Facebook/FacebookRequestException.php' );
+ require_once( 'Facebook/FacebookRequestException.php' );
 // require_once( 'Facebook/FacebookAuthorizationException.php' );
 // require_once( 'Facebook/GraphObject.php' );
-// require_once( 'Facebook/GraphUser.php' );
+ require_once( 'Facebook/GraphUser.php' );
 
 use Facebook\FacebookSession;
 			use Facebook\FacebookRequest;
