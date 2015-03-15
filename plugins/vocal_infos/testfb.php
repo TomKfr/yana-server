68748ca31d6f86960369a14888ed51eea26295d5
TomKfr <tkieffer67@gmail.com>
2015-03-15 20:41:12 +0100

    lol

--- plugins/vocal_infos/testfb.php
+++ plugins/vocal_infos/testfb.php
@@ -2,15 +2,16 @@
 
 
 define('FACEBOOK_SDK_V4_SRC_DIR', '/Facebook');
-require_once( 'Facebook/FacebookSession.php' );
-require_once( 'Facebook/FacebookRedirectLoginHelper.php' );
-require_once( 'Facebook/FacebookRequest.php' );
-require_once( 'Facebook/FacebookResponse.php' );
-require_once( 'Facebook/FacebookSDKException.php' );
-require_once( 'Facebook/FacebookRequestException.php' );
-require_once( 'Facebook/FacebookAuthorizationException.php' );
-require_once( 'Facebook/GraphObject.php' );
-require_once( 'Facebook/GraphUser.php' );
+require_once('autoload.php');
+// require_once( 'Facebook/FacebookSession.php' );
+// require_once( 'Facebook/FacebookRedirectLoginHelper.php' );
+// require_once( 'Facebook/FacebookRequest.php' );
+// require_once( 'Facebook/FacebookResponse.php' );
+// require_once( 'Facebook/FacebookSDKException.php' );
+// require_once( 'Facebook/FacebookRequestException.php' );
+// require_once( 'Facebook/FacebookAuthorizationException.php' );
+// require_once( 'Facebook/GraphObject.php' );
+// require_once( 'Facebook/GraphUser.php' );
 
 use Facebook\FacebookSession;
 			use Facebook\FacebookRequest;
