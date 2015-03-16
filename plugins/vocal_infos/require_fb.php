24486f1fd590d46fee96999e305ed3af70e8682d
TomKfr <tkieffer67@gmail.com>
2015-03-16 19:24:37 +0100

    new fich

---
+++ plugins/vocal_infos/require_fb.php
@@ -0,0 +1,11 @@
+<?php
+define('FACEBOOK_SDK_V4_SRC_DIR', '/var/www/yana-server/plugins/vocal_infos/src/Facebook/');
+require __DIR__ . '/autoload.php';
+
+use Facebook\FacebookSession;
+use Facebook\FacebookRequest;
+use Facebook\GraphUser;
+use Facebook\FacebookRequestException;
+use Facebook\FacebookCanvasLoginHelper;
+
+?>
