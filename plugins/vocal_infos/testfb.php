c2e6b539ee67a61d1ec7590ce131ccd47c70b1f5
TomKfr <tkieffer67@gmail.com>
2015-03-17 09:46:16 +0100

    corr bug

--- plugins/vocal_infos/testfb.php
+++ plugins/vocal_infos/testfb.php
@@ -22,6 +22,7 @@ use Facebook\FacebookCanvasLoginHelper;
 			// or create a FacebookSession with a valid access token:
 			$session = new FacebookSession('CAANa67rbsh4BAKPG5zbjbxhGpb80bKTJ1nYDChZAoveOI56y7ZBwRDDT2qBEY9CanZCnMmZCIrA6nZB5bwrGVImCr7OmJHoq8u8fEOsJT2p8KOtqZBn4fysLLSFtRjQvhTdCYZCYFZBdKhuw35PCcwS7gHqeiQcUK0nKfybQj3n5MdSdN4xzdZCo9ZAb5MNvHmxt3bArOnZC2uk5K0rVZAA5OZCl4');
 			
+			$ucount = 0;
 
 			// Get the GraphUser object for the current user:
 
